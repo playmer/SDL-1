b87e8f9b0af9938d35e340c51fcf8c45959ec3fc
Joshua T. Fisher <playmer@gmail.com>
2025-08-06 03:00:44 -0700

    more work

--- examples/video/01-menubar/menubar.c
+++ examples/video/01-menubar/menubar.c
@@ -84,7 +84,7 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv) {
 
   SDL_CreateWindowAndRenderer("menu bar test", 640, 480, 0, &window, &renderer);
 
-  //CreateMenuBar();
+  CreateMenuBar();
 
   //return SDL_APP_SUCCESS;
   return SDL_APP_CONTINUE;
