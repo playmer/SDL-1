594dc2a387b427b62fb3aefe321ce5cd3f53c983
Joshua T. Fisher <playmer@gmail.com>
2025-10-06 21:38:59 -0700

    Print out the app menu, retested the nil dance, unneeded.

--- examples/video/01-menubar/menubar.c
+++ examples/video/01-menubar/menubar.c
@@ -55,12 +55,6 @@ void PrintMenuItems(SDL_Renderer* renderer, SDL_MenuItem *menu_item, int indent,
         return;
     }
 
-    SDL_MenuItem *app_menu = SDL_GetMenuBarAppMenu(menu_item);
-    if (app_menu) {
-        SDL_RenderDebugText(renderer, (float)(8 * indent * 2), (float)(*total_index * 8), " -> AppMenu");
-        ++(*total_index);
-    }
-
     const char* label = SDL_GetMenuItemLabel(menu_item);
 
     if (!label) {
@@ -68,8 +62,24 @@ void PrintMenuItems(SDL_Renderer* renderer, SDL_MenuItem *menu_item, int indent,
     }
 
     SDL_RenderDebugText(renderer, (float)(8 * indent * 2), (float)(*total_index * 8), label);
-
     ++(*total_index);
+    
+    
+    if (SDL_GetMenuItemType(menu_item) == SDL_MENUITEM_MENUBAR) {
+        SDL_MenuItem *app_menu = SDL_GetMenuBarAppMenu(menu_item);
+        
+        if (app_menu) {
+            SDL_RenderDebugText(renderer, (float)(8 * (indent + 1) * 2), (float)(*total_index * 8), " -> AppMenu");
+            ++(*total_index);
+            
+            size_t item_count = SDL_GetMenuChildItems(app_menu);
+            
+            for (size_t i = 0; i < item_count; ++i) {
+                PrintMenuItems(renderer, SDL_GetMenuChildItem(app_menu, i), indent + 2, total_index);
+            }
+        }
+    }
+    
 
     size_t item_count = SDL_GetMenuChildItems(menu_item);
 
@@ -114,10 +124,10 @@ void CreateMenuBar_1()
 
         SDL_MenuItem* app_menu = SDL_GetMenuBarAppMenu(menu_bar_1);
         if (app_menu) {
-            SDL_assert(!SDL_CreateMenuItem(menu_bar_1, "Exit", SDL_MENUITEM_BUTTON, MENU_BAR_EXIT));
-            SDL_CreateMenuItem(app_menu, "Exit", SDL_MENUITEM_BUTTON, MENU_BAR_EXIT);
+            SDL_assert(!SDL_CreateMenuItem(menu_bar_1, "Exit 1", SDL_MENUITEM_BUTTON, MENU_BAR_EXIT));
+            SDL_CreateMenuItem(app_menu, "Exit 1", SDL_MENUITEM_BUTTON, MENU_BAR_EXIT);
         } else {
-            SDL_CreateMenuItem(menu_bar_1, "Exit", SDL_MENUITEM_BUTTON, MENU_BAR_EXIT);
+            SDL_CreateMenuItem(menu_bar_1, "Exit 1", SDL_MENUITEM_BUTTON, MENU_BAR_EXIT);
         }
     }
 
@@ -161,10 +171,10 @@ void CreateMenuBar_2()
 
         SDL_MenuItem* app_menu = SDL_GetMenuBarAppMenu(menu_bar_2);
         if (app_menu) {
-            SDL_assert(!SDL_CreateMenuItem(menu_bar_2, "Exit", SDL_MENUITEM_BUTTON, MENU_BAR_EXIT));
-            SDL_CreateMenuItem(app_menu, "Exit", SDL_MENUITEM_BUTTON, MENU_BAR_EXIT);
+            SDL_assert(!SDL_CreateMenuItem(menu_bar_2, "Exit 2", SDL_MENUITEM_BUTTON, MENU_BAR_EXIT));
+            SDL_CreateMenuItem(app_menu, "Exit 2", SDL_MENUITEM_BUTTON, MENU_BAR_EXIT);
         } else {
-            SDL_CreateMenuItem(menu_bar_2, "Exit", SDL_MENUITEM_BUTTON, MENU_BAR_EXIT);
+            SDL_CreateMenuItem(menu_bar_2, "Exit 2", SDL_MENUITEM_BUTTON, MENU_BAR_EXIT);
         }
     }
 
